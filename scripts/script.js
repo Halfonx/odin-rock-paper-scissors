02ed7fc12785cc15b0e11a1de3df5ee48184607d
Halfonx <jermy.prigent@gmail.com>
2023-11-04 23:29:20 +0100

    Create the player's choice

--- scripts/script.js
+++ scripts/script.js
@@ -2,4 +2,9 @@ let possibleChoices;
 possibleChoices = ["ROCK", "PAPER", "SCISSORS"]; // Value possible for the game, stored in an array
 let computerChoice;
 computerChoice = Math.floor(Math.random() * 3); // Assign a random value between 0 and 2 to the variable
-console.log(possibleChoices[computerChoice]);
+console.log(possibleChoices[computerChoice]); // Display the computer's choice
+
+let playerChoice;
+playerChoice = prompt("Choose between ROCK, PAPER or SCISSORS"); // Ask to the player the enter a value
+playerChoice =  playerChoice.toUpperCase(); // Set the player input to upper case
+console.log(playerChoice);
