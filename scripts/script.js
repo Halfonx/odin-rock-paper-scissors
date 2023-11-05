5940e1576607804ac2fdbdb3365b9b0ec9dd9d4b
Halfonx <jermy.prigent@gmail.com>
2023-11-05 20:27:48 +0100

    Set up once again the script file

---
+++ scripts/script.js
@@ -0,0 +1,12 @@
+let playerSelection; // Variable to store the player selection
+let computerSelection; // Variable to store the computer selection
+
+function getComputerChoice() { // Function to get the computer selection
+
+}
+
+function playRound(player, computer) { // Function who compares the player selection to the computer selection once and tells the winner
+
+}
+
+computerSelection = getComputerChoice();
