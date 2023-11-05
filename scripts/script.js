e6f5ae738223f621ca93a9de6c8777bdbc3ee66d
Halfonx <jermy.prigent@gmail.com>
2023-11-05 20:48:33 +0100

    Add the computer and player choice function

--- scripts/script.js
+++ scripts/script.js
@@ -1,12 +1,19 @@
 let playerSelection; // Variable to store the player selection
 let computerSelection; // Variable to store the computer selection
+let possibleChoices = ["ROCK", "PAPER", "SCISSORS"]; // An array with all the choices the computer can have
+let randomIndex = Math.floor(Math.random() * 3); // Generate a random number between 0 and 2
 
 function getComputerChoice() { // Function to get the computer selection
+    computerSelection = possibleChoices[randomIndex];
+}
 
+function askPlayerChoice() {
+    playerSelection = prompt("Choose between ROCK, PAPER and SCISSORS");
+    playerSelection = playerSelection.toUpperCase();
 }
 
 function playRound(player, computer) { // Function who compares the player selection to the computer selection once and tells the winner
-
+    return "Play round";
 }
 
 computerSelection = getComputerChoice();
