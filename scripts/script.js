5f6b6f7ec72b2dc856372a248e43de42c30e821f
Halfonx <jermy.prigent@gmail.com>
2023-11-06 22:38:05 +0100

    Add the error message if the computer is not valid

--- scripts/script.js
+++ scripts/script.js
@@ -8,13 +8,19 @@ function getComputerChoice() { // Function to get the computer selection
 }
 
 function playRound(player, computer) { // Function who compares the player selection to the computer selection once and tells the winner
-    if (player !== "ROCK" && player !== "PAPER" && player !== "SCISSORS") {
+    if (player !== "ROCK" && player !== "PAPER" && player !== "SCISSORS") { // Display an error message if the user don't enter a correct value
         return "Please enter a correct value. Reload the page";
     } else {
-        player = player.toUpperCase();
-        if (computer === undefined) { // If the computer selection is undefined, it calls the computer choice function and assign the value to the parameter
-            getComputerChoice();
-            computer = computerSelection;
+        if (computer !== undefined && computer !== "ROCK" && computer !== "PAPER" && computer !== "SCISSORS") { // Display an error message if the computer value is not correct
+            return "The computer value is not valid. Please enter a correct value or leave it empty. Reload the page";
+        } else {
+            player = player.toUpperCase();
+            if (computer === undefined) { // If the computer selection is undefined, it calls the computer choice function and assign the value to the parameter
+                getComputerChoice();
+                computer = computerSelection;
+            } else { // Do this if the computer value is defined
+                
+            }
         }
     }
 }
