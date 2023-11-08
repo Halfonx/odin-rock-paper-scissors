8acee0271cc7d6f14338fac02ca40da6ba6eac60
Halfonx <jermy.prigent@gmail.com>
2023-11-08 22:25:07 +0100

    Add the game function and the scores variable

--- scripts/script.js
+++ scripts/script.js
@@ -3,6 +3,8 @@ let computerSelection; // Variable to store the computer selection
 let possibleChoices = ["ROCK", "PAPER", "SCISSORS"]; // An array with all the choices the computer can have
 let randomIndex = Math.floor(Math.random() * 3); // Generate a random number between 0 and 2
 let result = "";
+let playerScore = 0;
+let computerScore = 0;
 
 function getComputerChoice() { // Function to get the computer selection
      computerSelection = possibleChoices[randomIndex];
@@ -10,21 +12,40 @@ function getComputerChoice() { // Function to get the computer selection
 }
 
 function changeRandomIndex() {
-    console.log(randomIndex);
     randomIndex = "";
     randomIndex = Math.floor(Math.random() * 3);
-    console.log(randomIndex);
 }
 
-function playRound(player, computer) { // Function who compares the player selection to the computer selection once and tells the winner
+function askPlayerChoice() {
+    playerSelection = prompt("Choose between ROCK, PAPER and SCISSORS");
+}
+
+function playRound() { // Function who compares the player selection to the computer selection once and tells the winner
     getComputerChoice();
-    computer = computerSelection;
-    if (player === computer) {
+    askPlayerChoice();
+    let computer = computerSelection;
+    let player = playerSelection;
+    if (player === computer) { // Cases for when the two parameters are equal
         result = "It's a tie! Nobody wins!";
-    } else if (player === "ROCK" && computer === "SCISSORS" || player === "PAPER" && computer === "ROCK" || player === "SCISSORS" && computer === "PAPER") {
+        return result;
+    } else if (player === "ROCK" && computer === "SCISSORS" || player === "PAPER" && computer === "ROCK" || player === "SCISSORS" && computer === "PAPER") { // Cases when the player wins
         result = `Player wins! ${player} beats ${computer}!`;
-    } else {
+        playerScore++;
+        playerScore = playerScore;
+        return result;
+    } else { // Cases when the computer wins
         result = `Computer wins! ${computer} beats ${player}!`;
+        computerScore++;
+        computerScore = computerScore;
+        return result;
     }
-    return result;
+}
+
+function game() {
+    for (i = 0; i <= 4; i++) {
+        changeRandomIndex();
+        playRound();
+        console.log(result);
+        console.log(`Score: Player ${playerScore} | Computer ${computerScore}`);
+    }
 }
