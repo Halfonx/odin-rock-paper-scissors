4f8e983b2be667d215d3e05d762a24c6821c700a
Halfonx <jermy.prigent@gmail.com>
2023-11-08 21:58:06 +0100

    Add the result variable and the changeRandomIndex function

--- scripts/script.js
+++ scripts/script.js
@@ -2,20 +2,29 @@ let playerSelection; // Variable to store the player selection
 let computerSelection; // Variable to store the computer selection
 let possibleChoices = ["ROCK", "PAPER", "SCISSORS"]; // An array with all the choices the computer can have
 let randomIndex = Math.floor(Math.random() * 3); // Generate a random number between 0 and 2
+let result = "";
 
 function getComputerChoice() { // Function to get the computer selection
      computerSelection = possibleChoices[randomIndex];
      return computerSelection;
 }
 
+function changeRandomIndex() {
+    console.log(randomIndex);
+    randomIndex = "";
+    randomIndex = Math.floor(Math.random() * 3);
+    console.log(randomIndex);
+}
+
 function playRound(player, computer) { // Function who compares the player selection to the computer selection once and tells the winner
     getComputerChoice();
     computer = computerSelection;
     if (player === computer) {
-        return "It's a tie! Nobody wins!";
+        result = "It's a tie! Nobody wins!";
     } else if (player === "ROCK" && computer === "SCISSORS" || player === "PAPER" && computer === "ROCK" || player === "SCISSORS" && computer === "PAPER") {
-        return `Player wins! ${player} beats ${computer}!`;
+        result = `Player wins! ${player} beats ${computer}!`;
     } else {
-        return `Computer wins! ${computer} beats ${player}!`;
+        result = `Computer wins! ${computer} beats ${player}!`;
     }
+    return result;
 }
